f2415189f51e7634ac250c2c5e2f075369ec66fb
Rehan-Sattar <rehansattar117@gmail.com>
2019-01-30 23:28:07 +0500

    content updations

--- db.structure.js
+++ db.structure.js
@@ -1,34 +1,3 @@
-let authCheck = () => {
-  if (!localStorage.getItem('student')) {
-    window.location.href = '../../../index.html'
-  }
-}
-
-let courseOverview = () => {
-  localStorage.setItem('data', JSON.stringify(overViewContent))
-  window.location.href = '../html-course/content-page.html'
-}
-
-let courseHtmlCourse = () => {
-  let arrayOfContent = ['TABLE', 'HEADER', 'IMG', 'P']
-  localStorage.setItem('data', JSON.stringify(arrayOfContent))
-  window.location.href = '../html-course/content-page.html'
-}
-
-let courseChallenges = () => {
-  let arrayOfContent = ['CHALLENGES', 'CHALLENGES', 'CHALLENGES', 'CHALLENGES']
-  localStorage.setItem('data', JSON.stringify(arrayOfContent))
-  window.location.href = '../html-course/content-page.html'
-}
-
-let courseHtml5 = () => {
-  let arrayOfContent = ['HTML5', 'HTML5', 'HTML5', 'HTML5']
-  localStorage.setItem('data', JSON.stringify(arrayOfContent))
-  window.location.href = '../html-course/content-page.html'
-}
-
-// ==================== DATA =============================
-
 const overViewContent = {
   HTML: {
     title: 'HTML TILE',
