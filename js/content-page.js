7e927b1d962b0ee393daad924753d5ec3386cf37
soha-moosa <soha.moosa@yahoo.com>
2019-01-31 10:01:51 +0500

    fixed bug

--- js/content-page.js
+++ js/content-page.js
@@ -1,52 +1,58 @@
 let logout = () => {
-  localStorage.removeItem('student')
-  window.location.href = '../../../index.html'
+    localStorage.removeItem('student')
+    window.location.href = '../../../index.html'
 }
 function hello() {
-  window.location.href = 'test.html'
+    window.location.href = 'test.html'
 }
 let authCheckAndShowContent = () => {
-  if (!localStorage.getItem('student')) {
-    window.location.href = '../../../index.html'
-  }
-
-  let titles = []
-  if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsOverview = JSON.parse(localStorage.getItem('data'))
-
-    for (key in topicsOverview) {
-      titles.push(key)
+    if (!localStorage.getItem('student')) {
+        window.location.href = '../../../index.html'
     }
 
-    printToDom(titles)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsHtmlBasics = JSON.parse(localStorage.getItem('data'))
+    let titles = []
+    if (JSON.parse(localStorage.getItem('data'))) {
+        const topicsOverview = JSON.parse(localStorage.getItem('data'))
 
-    // printToDom(topicsHtmlBasics)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsChallenges = JSON.parse(localStorage.getItem('data'))
+        for (key in topicsOverview) {
+            titles.push(key)
+        }
 
-    // printToDom(topicsChallenges)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsHTML5 = JSON.parse(localStorage.getItem('data'))
+        printToDom(titles)
+    } else if (JSON.parse(localStorage.getItem('data'))) {
+        const topicsHtmlBasics = JSON.parse(localStorage.getItem('data'))
 
-    // printToDom(topicsHTML5)
-    const data = JSON.parse(localStorage.getItem('data'))
-    printToDom()
-  }
+        // printToDom(topicsHtmlBasics)
+    } else if (JSON.parse(localStorage.getItem('data'))) {
+        const topicsChallenges = JSON.parse(localStorage.getItem('data'))
+
+        // printToDom(topicsChallenges)
+    } else if (JSON.parse(localStorage.getItem('data'))) {
+        const topicsHTML5 = JSON.parse(localStorage.getItem('data'))
+
+        // printToDom(topicsHTML5)
+        const data = JSON.parse(localStorage.getItem('data'))
+        printToDom()
+    }
 }
 // object => loop
 
 // looping on object
 
 let printToDom = titles => {
-  titles.map(title => {
-    document.getElementById('courseContentDiv').innerHTML += `  
-            <div class="col l4 s12 m12">
-                    <div class="card black-grey darken-1">
+    titles.map(title => {
+        document.getElementById('courseContentDiv').innerHTML += `  
+            <div class="col l4 s12 m12" style="cursor: pointer;" onclick="setItemToLocalStorage('${title}', './test.html')">
+                    <div class="card hoverable z-depth-3 black-grey darken-1">
                         <div class="card-content black-text">
                             <h1 class="card-title">${title}</h1>
+                        </div>
                     </div>
                 </div>     `
-  })
+    })
+}
+
+const setItemToLocalStorage = (item, path) => {
+    localStorage.setItem('test-content', item);
+    window.location.href = path;
 }
--- js/test.js
+++ js/test.js
@@ -1,64 +1,108 @@
 let logout = () => {
-  localStorage.removeItem('student')
-  window.location.href = '../../../index.html'
+    localStorage.removeItem('student')
+    window.location.href = '../../../index.html'
 }
 function hello() {
-  window.location.href = 'test.html'
+    window.location.href = 'test.html'
 }
 let authCheckAndShowContent = () => {
-  if (!localStorage.getItem('student')) {
-    window.location.href = '../../../index.html'
-  }
-
-  if (JSON.parse(localStorage.getItem('data'))) {
-    const data = JSON.parse(localStorage.getItem('data'))
-    console.log(data)
-    var propValue
-    for (var propName in data) {
-      propValue = data[propName]
-      console.log(propName, propValue)
-      console.log(propValue.title)
-      console.log(propValue.description)
-
-      document.getElementById('title').innerHTML = propValue.title
-      document.getElementById('description').innerHTML = propValue.title
-    }
-    //   for (key in data) {
-    //     document.getElementById('courseContentDiv').innerHTML += '<div class="col l4 s12 m12"><div class="card black-grey darken-1"><div class="card-content black-text"><h1 class="card-title">' + data + '</h1></div></div>'
-    //     console.log(key, data[key])
-    //   }
-    // printToDom(topicsOverview)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsHtmlBasics = JSON.parse(localStorage.getItem('data'))
-
-    for (key in topicsOverview) {
-      console.log(key, topicsOverview[key])
+    if (!localStorage.getItem('student')) {
+        window.location.href = '../../../index.html'
     }
 
-    // printToDom(topicsHtmlBasics)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsChallenges = JSON.parse(localStorage.getItem('data'))
-
-    // printToDom(topicsChallenges)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsHTML5 = JSON.parse(localStorage.getItem('data'))
-
-    // printToDom(topicsHTML5)
-  }
+    if (JSON.parse(localStorage.getItem('data'))) {
+        const data = JSON.parse(localStorage.getItem('data'))
+        const item = localStorage.getItem('test-content');
+        console.log(data, item);
+        let content = []
+        for (let key in data) {
+            if (key === item) {
+                content.push(data[key])
+                printToDom(content, 'print-content')
+            }
+        }
+    }
 }
 
 // object => loop
 
 // looping on object
 
-let printToDom = array => {
-  array.map(topic => {
-    document.getElementById('courseContentDiv').innerHTML += `  
-              <div class="col l4 s12 m12">
-                      <div class="card black-grey darken-1">
-                          <div class="card-content black-text">
-                              <h1 class="card-title">${topic}</h1>
-                      </div>
-                  </div>     `
-  })
+let printToDom = (array, flag) => {
+    if (flag === "print-content") {
+        array.map(topic => {
+
+            localStorage.setItem('test-question', JSON.stringify(topic.test))
+            document.getElementById('quizDiv').innerHTML =
+                `
+        <h3 class="content" id="title">${topic.title}</h3>
+            <p class="content" id="description">
+               ${topic.description}
+            </p>
+            <code class="container center purple-text grey lighten-2"> ${topic.code}</code>
+            <div style="margin-left: 70%; margin-top:2%;">
+                <a href="ide.html" class="waves-effect pink waves-light btn"><i class="material-icons right">code</i>Try
+                    Yourself</a>
+            </div>
+
+            <div style="margin-top:10%; padding: 2%;" class="center container yellow lighten-2">
+                <p class="black-text">
+                    ${topic.note}
+                </p>
+            </div>
+
+            <div style="margin-top:2%;" class="container center">
+                <button class="btn-floating btn-large waves-effect waves-light pink" onclick="setTestContent()"><i class="material-icons">play_arrow</i></button>
+                   </div>
+
+        `
+        })
+    } else if (flag === "test-questions") {
+        console.log('IM here')
+        array.map(topic => {
+            console.log()
+            localStorage.setItem('test-question', JSON.stringify(topic.test))
+            document.getElementById('quizDiv').innerHTML =
+                `
+                <p class="black-text">${topic.question}</p>
+                 <form>
+    <p>
+      <label>
+        <input name="group1" type="radio" checked />
+        <span class="black-text">${topic..options[0]}</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group1" type="radio" />
+        <span>Yellow</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input class="with-gap" name="group1" type="radio"  />
+        <span>Green</span>
+      </label>
+    </p>
+    <p>
+      <label>
+        <input name="group1" type="radio" disabled="disabled" />
+        <span>Brown</span>
+      </label>
+    </p>
+  </form>
+        `
+        })
+    }
+
+}
+
+const setTestContent = () => {
+    let testArray = []
+    const test = JSON.parse(localStorage.getItem('test-question'))
+    console.log(test)
+    testArray.push(test)
+    printToDom(testArray, 'test-questions')
+
+
 }
