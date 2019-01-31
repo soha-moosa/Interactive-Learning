8e5e90b23af7fb1c750827252c8da704ceb31d74
nehaejaz <neha.ejaz@icloud.com>
2019-01-31 05:23:57 +0500

    done

--- js/content-page.js
+++ js/content-page.js
@@ -2,21 +2,34 @@ let logout = () => {
   localStorage.removeItem('student')
   window.location.href = '../../../index.html'
 }
-
+function hello(){
+  window.location.href= "test.html";
+}
 let authCheckAndShowContent = () => {
   if (!localStorage.getItem('student')) {
     window.location.href = '../../../index.html'
   }
 
   if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsOverview = JSON.parse(localStorage.getItem('data'))
+    const data = JSON.parse(localStorage.getItem('data'))
+
+    var propValue;
+    for (var propName in data) {
+      propValue = data[propName]
+      console.log(propName,propValue);
+      document.getElementById('courseContentDiv').innerHTML += '<div class="col l4 s12 m12"><div class="card black-grey darken-1"><div class="card-content black-text"><h1 class="card-title">' + propName + '</h1></div><button class="pink btn" onclick="hello()">Start</button</div>'
+    }
+    // for (key in data) {
+    //   document.getElementById('courseContentDiv').innerHTML += '<div class="col l4 s12 m12"><div class="card black-grey darken-1"><div class="card-content black-text"><h1 class="card-title">' + data + '</h1></div></div>'
+    //   console.log(key, data[key])
+    // }
+    // printToDom(topicsOverview)
+  } else if (JSON.parse(localStorage.getItem('data'))) {
+    const topicsHtmlBasics = JSON.parse(localStorage.getItem('data'))
 
     for (key in topicsOverview) {
       console.log(key, topicsOverview[key])
     }
-    // printToDom(topicsOverview)
-  } else if (JSON.parse(localStorage.getItem('data'))) {
-    const topicsHtmlBasics = JSON.parse(localStorage.getItem('data'))
 
     // printToDom(topicsHtmlBasics)
   } else if (JSON.parse(localStorage.getItem('data'))) {
@@ -42,7 +55,6 @@ let printToDom = array => {
                         <div class="card-content black-text">
                             <h1 class="card-title">${topic}</h1>
                     </div>
-                </div>
-    `
+                </div>     `
   })
 }
